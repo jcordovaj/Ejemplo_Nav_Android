4af3787094e3286b523b81270881a5dbdc471e11
Jorge Cordova <55065873+jcordovaj@users.noreply.github.com>
2025-09-11 17:42:47 -0300

    Creación patron de navegación, fragments, etc
    Configuración de:
    1) Resource Manager, New resource file
    2) En res, crear nuevo MENU, (Android resource Directory)
    3) En Menu, agregar un nuevo "Resource File", "barra_de_navegación"
    4) Agregar objetos gráficos "favoritos" y "musica"
    5) Crear nuevo componente "Fragment", desde el paquete, botón derecho agregar "New Android Component", uno para música y el otro para Favorito
    6) Para usar los fragmentos se agrega en el activity main, un "fragment container view", es código
    7) Luego se va a navigation/nav, canvas, con el ícono "+" se agregan las vistas creadas (favoritos y musica).
    8) Revisar build.gradle.kts (proyecto) y agregar id("androidx.navigation,safeguards") version "2.8.2" apply false
    9) Revisar build.gradle.kts (app) y agregar id("androidx.navigation,safeargs")

---
+++ app/src/main/java/com/ejemplos/android_nav_paso_a_paso/Favoritos.java
@@ -0,0 +1,56 @@
+package com.ejemplos.android_nav_paso_a_paso;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class Favoritos extends Fragment {
+
+    private EditText text_1, text_2;
+    private Button btn;
+
+
+
+    public Favoritos() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_favoritos, container, false);
+
+        text_1 = view.findViewById(R.id.text1);
+        text_2 = view.findViewById(R.id.text2);
+        btn = view.findViewById(R.id.button);
+
+        btn.setOnClickListener( v ->{
+            int num1 = Integer.parseInt(text_1.getText().toString());
+            int num2 = Integer.parseInt(text_2.getText().toString());
+
+            FavoritosDirections.ActionFavoritosToMusica2 action =
+                    FavoritosDirections.actionFavoritosToMusica2();
+            action.setNumero1(num1);
+            action.setNumero2(num2);
+
+            NavHostFragment.findNavController(Favoritos.this).navigate(action);
+
+        });
+        return view;
+    }
+}
