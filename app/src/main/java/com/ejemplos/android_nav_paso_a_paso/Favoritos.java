b8683c2a28a787d8e926d3af882b7eb347374ee2
Jorge Cordova <55065873+jcordovaj@users.noreply.github.com>
2025-09-13 21:13:40 -0300

    correcciones

--- app/src/main/java/com/ejemplos/android_nav_paso_a_paso/Favoritos.java
+++ app/src/main/java/com/ejemplos/android_nav_paso_a_paso/Favoritos.java
@@ -11,7 +11,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
-
 public class Favoritos extends Fragment {
 
     private EditText text_1, text_2;
@@ -20,7 +19,7 @@ public class Favoritos extends Fragment {
     public Favoritos() {
         // Required empty public constructor
     }
-    
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,9 +46,7 @@ public class Favoritos extends Fragment {
 
             NavHostFragment.findNavController(Favoritos.this).navigate(action);
 
-
         });
-
 
         return view;
     }
